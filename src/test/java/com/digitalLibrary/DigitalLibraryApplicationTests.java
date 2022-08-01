1022d770b445706d8a97599865b5ba9d9bf9689b
Anika-Tahasin <anikatahasinprapti@gmail.com>
2022-08-01 21:06:12 +0600

    This is Home page

---
+++ src/test/java/com/digitalLibrary/DigitalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
